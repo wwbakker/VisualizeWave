d8a15897ffca45917f41acbda363905f54e1eab3
Wessel W. Bakker <wwbakker@gmail.com>
2021-12-31 12:04:55 +0100

    Add project files.

---
+++ VisualizeWave/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WAV_Tools_C_Sharp;
+using System.Media;
+using Path = System.IO.Path;
+
+namespace VisualizeWave
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly String waveFilePath;
+        private readonly WAV_file wavFile;
+        private UInt16[] samples;
+        public MainWindow()
+        {
+            waveFilePath = @"E:\Dev\Repos\sampleaudio\output.wav";
+            wavFile = new WAV_file(Path.GetDirectoryName(waveFilePath), Path.GetFileName(waveFilePath));
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            wavFile.loadFile();
+            wavFile.getBuffer_16_bits_mono(out samples);
+            LoadBitmap();
+        }
+
+        WriteableBitmap writeableBitmap;
+        protected void LoadBitmap()
+        {
+
+            RenderOptions.SetBitmapScalingMode(WaveImage, BitmapScalingMode.NearestNeighbor);
+            RenderOptions.SetEdgeMode(WaveImage, EdgeMode.Aliased);
+
+            writeableBitmap = new WriteableBitmap(
+                (int)ActualWidth,
+                (int)ActualHeight,
+                96,
+                96,
+                PixelFormats.Bgr32,
+                null);
+
+            WaveImage.Source = writeableBitmap;
+
+            /*WaveImage.Stretch = Stretch.Fill;*/
+            WaveImage.HorizontalAlignment = HorizontalAlignment.Left;
+            WaveImage.VerticalAlignment = VerticalAlignment.Top;
+
+            try
+            {
+                writeableBitmap.Lock();
+                if (samples != null && WaveImage.ActualWidth > 0)
+                {
+                    var scaleWidth = samples.Length / WaveImage.ActualWidth;
+                    var sampleResolution = wavFile.BitsPerSample == WAV_file.BITS_PER_SAMPLE.BPS_8_BITS ? Byte.MaxValue : UInt16.MaxValue;
+                    var scaleHeight = WaveImage.ActualHeight / sampleResolution;
+
+                    unsafe
+                    {
+                        // Get a pointer to the back buffer.
+                        IntPtr pBackBuffer = writeableBitmap.BackBuffer;
+                        var pPixel = (int x, int y) => pBackBuffer + (y * writeableBitmap.BackBufferStride) + (x * 4);
+                        int color_data = 0x00ff00;
+                        
+                        for (int i = 0; i < WaveImage.ActualWidth; i++)
+                        {
+                            var x = i;
+                            var y = (int)(samples[(int)Math.Min(i * scaleWidth, samples.Length - 1)] * scaleHeight);
+                            * ((int*)pPixel(x, y)) = color_data;
+                        }
+
+                    }
+
+                    writeableBitmap.AddDirtyRect(new Int32Rect(0, 0, (int)WaveImage.ActualWidth, (int)WaveImage.ActualHeight));
+                }
+            }
+            finally
+            {
+                // Release the back buffer and make it available for display.
+                writeableBitmap.Unlock();
+            }
+
+        }
+
+
+        private void WaveImage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            LoadBitmap();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                var soundPlayer = new SoundPlayer(waveFilePath);
+                soundPlayer.Play();
+            }
+        }
+    }
+}
---
+++ VisualizeWave/io/WavFile.cs
@@ -0,0 +1,54 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualizeWave.io
+{
+    internal class WavFile
+    {
+        private static readonly byte[] RIFF_HEADER = new byte[] { 0x52, 0x49, 0x46, 0x46 };
+        private static readonly byte[] FORMAT_WAVE = new byte[] { 0x57, 0x41, 0x56, 0x45 };
+        private static readonly byte[] FORMAT_TAG = new byte[] { 0x66, 0x6D, 0x74, 0x20 };
+        private static readonly byte[] AUDIO_FORMAT = new byte[] { 0x1, 0x0 };
+        private static readonly byte[] SUBCHUNK_ID = new byte[] { 0x64, 0x61, 0x74, 0x61 };
+        private static readonly int HEADER_SIZE_BYTES = 40;
+        public int ChunkSize
+        {
+            get
+            {
+                return BitConverter.ToInt32(Data, 4);
+            }
+        }
+
+        public int ChannelCount { get; private set; }
+
+        private readonly String path;
+
+        private byte[]? _data = null;
+        private byte[] Data
+        {
+            get
+            {
+                if (_data == null)
+                {
+                    LoadFromDisk();
+                }
+                return _data ?? Array.Empty<byte>();
+            }
+        }
+
+        public WavFile(String path)
+        {
+            this.path = path;
+        }
+
+        public void LoadFromDisk()
+        {
+            _data = File.ReadAllBytes(path);
+        }
+    }
+}
+*/
